a1a56e2ff87ad300f5ca14f0ee0c03527ae4bfef
SAHIL <84861578+sahil5758@users.noreply.github.com>
2024-02-17 20:37:47 +0530

    First Index.cpp

---
+++ First Index.cpp
@@ -0,0 +1,14 @@
+vector<int> Solution::solve(vector<int> &a, vector<int> &b) {
+      vector<int> v;
+    for(int i=0; i<b.size(); i++){
+        int ans=-1;
+        for(int j=0; j<a.size(); j++){
+            if(a[j]>=b[i]){
+                ans=j;
+                break;
+            }
+        }
+        v.push_back(ans);
+    }
+    return v;
+}
