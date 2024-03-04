b654ab6143a1dae18f468621663274cd30d34977
SAHIL <84861578+sahil5758@users.noreply.github.com>
2024-03-04 23:26:28 +0530

    reverse the string.cpp

---
+++ reverse the string.cpp
@@ -0,0 +1,14 @@
+string Solution::reverseString(string A) {
+stack <char>s;
+for(int i=0;i<A.size();i++)
+{
+s.push(A[i]);
+}
+for(int i=0;i<A.size();i++)
+{
+A[i]=s.top();
+s.pop();
+}
+return A;
+    
+}
